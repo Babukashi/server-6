0e8931a004627f7c140924817c71e5bd6e52c152
Babukashi <206505264+Babukashi@users.noreply.github.com>
2025-11-07 12:06:48 +0900

    hwnkou

--- server.js
+++ server.js
@@ -2,3 +2,13 @@
 const kv = await Deno.openKv();
 
 await kv.set(['pokemon', 'アマルス'], { type: 'こおり', level: 30 });
+
+const pkmn = await kv.get(['pokemon', 'アマルス']);
+console.log(pkmn);
+
+const KvListIterator = await kv.list([{ prefix: pokemon }]);
+
+for await (const pkmn of pkmns) {
+  console.log(pkmn.key);
+  console.log(pkmn.value);
+}
